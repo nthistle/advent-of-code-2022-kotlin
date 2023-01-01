453671d5913a5843eb61ef21904c762d9a7dad42
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2023-01-01 17:38:28 -0500

    Tidy up day 1

--- day01/Solve.kt
+++ day01/Solve.kt
@@ -3,7 +3,10 @@ import java.io.File
 fun main() {
     val input =
         File("input.txt").readText().trim().split("\n\n").map { it.lines().map { it.toInt() } }
-    val totalPerElf = input.map { it.sum() }
-    println(totalPerElf.max())
-    println(totalPerElf.sorted().takeLast(3).sum())
+    input
+        .map { it.sum() }
+        .let {
+            println(it.max())
+            println(it.sorted().takeLast(3).sum())
+        }
 }
