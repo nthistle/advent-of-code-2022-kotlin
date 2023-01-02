0f0aba6829f1c7191229226c87628c166f2f352c
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2023-01-01 22:44:25 -0500

    Format days 5 and 6

--- day05/Solve.kt
+++ day05/Solve.kt
@@ -1,43 +1,48 @@
 import java.io.File
 
-data class Operation(
-    val count: Int,
-    val src: Int,
-    val dst: Int
-)
+data class Operation(val count: Int, val src: Int, val dst: Int)
 
 fun nums(s: String): Sequence<Int> = Regex("-?\\d+").findAll(s).map { it.value.toInt() }
 
 fun main() {
-    val (initialCrates, rawOperations) = File("input.txt").readText().trim().split("\n\n").map { it.lines() }
-    val crateConfig = initialCrates.dropLast(1).map {
-        it.chunked(4).map { it[1] }
-    }.let { crates ->
-        val n = crates.last().size
-        (0..(n-1)).map { i -> crates.map { it[i] }.filter { it != ' ' }.reversed() }
-    }
-    val operations = rawOperations.map {
-        val (count, src, dst) = nums(it).toList()
-        Operation(count, src - 1, dst - 1)
-    }
+    val (initialCrates, rawOperations) =
+        File("input.txt").readText().trim().split("\n\n").map { it.lines() }
+    val crateConfig =
+        initialCrates
+            .dropLast(1)
+            .map { it.chunked(4).map { it[1] } }
+            .let { crates ->
+                val n = crates.last().size
+                (0..(n - 1)).map { i -> crates.map { it[i] }.filter { it != ' ' }.reversed() }
+            }
+    val operations =
+        rawOperations.map {
+            val (count, src, dst) = nums(it).toList()
+            Operation(count, src - 1, dst - 1)
+        }
 
-    println(crateConfig.map { it.toMutableList() } .let {  crates ->
-        operations.forEach {
-            operation -> 
-                repeat(operation.count) {
-                    crates[operation.dst].add(crates[operation.src].removeLast())
+    println(
+        crateConfig
+            .map { it.toMutableList() }
+            .let { crates ->
+                operations.forEach { operation ->
+                    repeat(operation.count) {
+                        crates[operation.dst].add(crates[operation.src].removeLast())
+                    }
                 }
-        }
-        crates.map { it.last() }.joinToString("")
-    })
+                crates.map { it.last() }.joinToString("")
+            }
+    )
 
-    println(crateConfig.map { it.toMutableList() } .let {  crates ->
-        operations.forEach {
-            operation -> 
-                crates[operation.dst].addAll(crates[operation.src].takeLast(operation.count))
-                repeat(operation.count) { crates[operation.src].removeLast() }
-        }
-        crates.map { it.last() }.joinToString("")
-    })
-
+    println(
+        crateConfig
+            .map { it.toMutableList() }
+            .let { crates ->
+                operations.forEach { operation ->
+                    crates[operation.dst].addAll(crates[operation.src].takeLast(operation.count))
+                    repeat(operation.count) { crates[operation.src].removeLast() }
+                }
+                crates.map { it.last() }.joinToString("")
+            }
+    )
 }
--- day06/Solve.kt
+++ day06/Solve.kt
@@ -2,6 +2,6 @@ import java.io.File
 
 fun main() {
     val input = File("input.txt").readText().trim()
-    println(4 + input.windowed(4).indexOfFirst( { it.toSet().size == it.length } ))
-    println(14 + input.windowed(14).indexOfFirst( { it.toSet().size == it.length } ))
+    println(4 + input.windowed(4).indexOfFirst({ it.toSet().size == it.length }))
+    println(14 + input.windowed(14).indexOfFirst({ it.toSet().size == it.length }))
 }
